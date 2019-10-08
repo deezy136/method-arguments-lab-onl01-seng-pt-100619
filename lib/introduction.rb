efb8a5da930b8f52b2d3cf3d0bf5b66d4f527777
deezy13 <deezy13@gmail.com>
2019-10-08 04:12:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,7 @@ def introduction_with_language(name)
   puts greet
   
 end 
+=begin
 def introduction_with_language(name, language)
   
   greet = "Hi, my name is #{name} and I am learning to program in #{language}."
@@ -13,3 +14,4 @@ def introduction_with_language(name, language)
   puts greet
   
 end 
+=end 
